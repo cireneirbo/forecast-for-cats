3b2b6fc1034cd8d0ce3ac2b70a06fe0f890a7bb3
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-05-18 15:40:20 -0400

    Returning weather data, but need to work out hiding API key

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -1,40 +1,24 @@
 import axios from 'axios';
-//import fs from 'fs';
+// import fs from 'fs';
+// import env from "dotenv";
+// env.config();
+
 //require('dotenv').config();
 
 function Weather() {
   
+  // const { OPEN_WEATHER_MAPS_API_KEY } = process.env;
+
+  const apiKey = "placeholder";
   async function getWeather() {
-    const url = "https://api.weather.gov";
-    const radarStation = "/radar/stations"
-    const stationID = "/KTBW";
-    // try {
-    //   const response = await axios.get(url + radarStation + stationID);
-      //console.log(response);
-      // Finding A Weather Station
-      // for(let i = 0; i < response.data.features.length; i++) {
-      //   if(response.data.features[i].properties.name.includes("Tampa")){
-      //     console.log(i); // returns 169 for Tampa
-      //     console.log(response.data.features[169]); 
-      //   }
-      // }
-
-      
-    // } catch (error) {
-    //   console.error(error);
-    // }
-
-    const zone = "/zones/forecast/FLZ255";
+    const lat = "27.49";
+    const lon = "-82.57";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    //console.log(process.env.development.OPEN_WEATHER_MAPS_API_KEY);
     try {
-      const response = await axios.get(url + zone);
+      const response = await axios.get(url);
       console.log(response);
-      // Finding A Weather Station
-      // for(let i = 0; i < response.data.features.length; i++) {
-      //   if(response.data.features[i].properties.name.includes("Tampa")){
-      //     console.log(i); // returns 169 for Tampa
-      //     console.log(response.data.features[1176]); 
-      //   }
-      // }
+      
 
       
     } catch (error) {
@@ -43,7 +27,7 @@ function Weather() {
   }
   getWeather();
   return (
-    <div className="Accuweather">
+    <div className="Weather">
       <p>Weather</p>
     </div>
   );
