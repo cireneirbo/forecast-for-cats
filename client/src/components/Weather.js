e9471d8491dcd45e5908984b88430f81fe34c7f1
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-05-21 18:41:35 -0400

    Add weather table and convertKtoF function

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -1,36 +1,60 @@
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-// import fs from 'fs';
-// import env from "dotenv";
-// env.config();
-
-//require('dotenv').config();
 
 function Weather() {
   
-  // const { OPEN_WEATHER_MAPS_API_KEY } = process.env;
+  const [ isProcessed, setIsProcessed ] = useState(false);
 
-  const apiKey = "placeholder";
-  async function getWeather() {
-    const lat = "27.49";
-    const lon = "-82.57";
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    //console.log(process.env.development.OPEN_WEATHER_MAPS_API_KEY);
-    try {
-      const response = await axios.get(url);
-      console.log(response);
-      
+  const [ data, setData ] = useState("");
 
-      
-    } catch (error) {
-      console.error(error);
+  const backendURL = `http://localhost:9000/weather`;
+
+  // converting Kelvin temp to Fahrenheit temp: F =(K - 273.15)* 1.8000+ 32.00
+
+  useEffect(() => {
+    if(isProcessed == false) {
+      axios.get(backendURL)
+      .then(res => {
+        setData(res.data);
+      }).catch(err => {
+        console.log(err);
+      });
+      return setIsProcessed(true);
     }
+  }, []);
+
+  function convertKelvinToFahrenheit(kelvin) {
+    return Math.round((1.8 * (kelvin - 273.15)) + 32);
   }
-  getWeather();
-  return (
-    <div className="Weather">
-      <p>Weather</p>
-    </div>
-  );
+
+
+  console.log(data);
+
+  if(data == "") {
+    return (
+      <main>
+        <div className="box-shadow">
+          <p>Awaiting API data...</p>
+        </div>
+      </main>
+    );
+  } else {
+    return (
+  
+      <div className="Weather">
+        <h3>{data.title}</h3>
+        <p>For lat: {data.weather_data.coord.lat} and lon: {data.weather_data.coord.lon}</p>
+        <table>
+          <tbody>
+            <tr>
+              <th>Feels Like</th>
+              <td>{convertKelvinToFahrenheit(data.weather_data.main.feels_like)} F</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Weather;
