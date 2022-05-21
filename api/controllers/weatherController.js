5d6e92b134779fd0c528df03d2510dfec24d4a79
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-05-21 15:04:20 -0400

    Getting good data back and displaying it as json

--- api/controllers/weatherController.js
+++ api/controllers/weatherController.js
@@ -5,35 +5,26 @@ require("dotenv").config();
 
 // weather API
 exports.index = function(req, res) {
-  let weatherData = {};
+
   async function getWeather() {
+    
     const apiKey = process.env.OPEN_WEATHER_MAPS_API_KEY;
     const lat = "27.49";
     const lon = "-82.57";
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     
-    
     try {
       const response = await axios.get(url);
-      console.log(response.data);
-      weatherData = response.data;
-      // return response.data;
+      // console.log(response.data);
+      res.send( { title: 'Weather', weather_data: response.data} );
       
     } catch (error) {
       next(error);
     }
 
-    
   }
 
-  getWeather();
-  if (weatherData != {}) {
-    res.json( { title: 'Weather', weather_data: weatherData} );
-    console.log(weatherData);
-  }
-  console.log(weatherData);
-  
-  
+  getWeather(); 
     
 };
 
