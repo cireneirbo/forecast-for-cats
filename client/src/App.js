b46db8217d21bbb4b4ae291612b387d4cbb92be5
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-05-25 23:18:49 -0400

    Add a cat

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import SunriseSunset from './components/SunriseSunset';
 import Weather from './components/Weather';
+import pixelCat from './pixel_cat.PNG';
 
 function App() {
   return (
     <div className="App">
+      <img src={pixelCat} className="images" alt="pixel cat"/>
       <Weather />
+      
       <SunriseSunset />
       
     </div>
