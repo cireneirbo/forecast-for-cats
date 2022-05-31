ab4d42a67dd59af6139666c2c608b9c7ba73f686
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-05-30 23:55:39 -0400

    Remove background and hone in media queries meore

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
       <div className="CatDialogue">
         <SunriseSunset />
         <Weather />
+        <img id="pixelCat" src={pixelCat} />
         <p className="dev-plug">Look at what else my <a href="https://linktr.ee/codeethicallywitheric" target="_blank">owner</a> does!</p>
       </div>
     </div>
